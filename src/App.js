89882278aa0650e9b26b81fae6ef202482882667
Jakub Gmur <jakubgmur@op.pl>
2023-07-18 08:23:44 +0200

    replaced default App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
 function App() {
+  const [portfolio, setPortfolio] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CoinAPI Cryptocurrency Portfolio Tracker App</h1>
     </div>
   );
 }
-
 export default App;
