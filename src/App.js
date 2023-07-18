bc431c6c41321e2716ee2791a1c5018c20382cda
Jakub Gmur <jakubgmur@op.pl>
2023-07-18 08:45:03 +0200

    code to fetch exchange rates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function App() {
   const [portfolio, setPortfolio] = useState([]);
+  useEffect(() => {
+    async function fetchExchangeRates() {
+      const assets = ['BTC', 'ETH', 'XRP'];
+      const promises = assets.map(asset =>
+        axios.get(`https://rest.coinapi.io/v1/exchangerate/${asset}/USD?apikey=YOUR_API_KEY`)
+      );
+      const responses = await Promise.all(promises);
+      const exchangeRates = responses.reduce((acc, response, index) => {
+        acc[assets[index]] = response.data.rate;
+        return acc;
+      }, {});
+      setPortfolio(exchangeRates);
+    }
+    fetchExchangeRates();
+  }, []);
   return (
     <div className="App">
-      <h1>CoinAPI Cryptocurrency Portfolio Tracker App</h1>
+     <h1>CoinAPI Cryptocurrency Portfolio Tracker App</h1>
+     <ul>
+        {Object.entries(portfolio).map(([asset, exchangeRate]) => (
+          <li key={asset}>{asset}: {exchangeRate}</li>
+        ))}
+      </ul>
     </div>
   );
 }
